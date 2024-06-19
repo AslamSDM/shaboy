64e85812effaef9f3db24e4d8428e1d18bd736bd
AslamSDM <aslamprpd@gmail.com>
2024-06-20 01:05:32 +0530

    update

--- packages/nextjs/components/emulator/fetch-rom.tsx
+++ packages/nextjs/components/emulator/fetch-rom.tsx
@@ -1,43 +1,54 @@
 "use client";
 
+import axios from 'axios';
 import React, { useContext, useMemo, useState, FunctionComponent, SetStateAction, Dispatch } from 'react';
 import { GbaContext } from 'react-gbajs';
 
 type Props = { setRomLoaded: Dispatch<SetStateAction<boolean>> }
 
-const FetchROM: FunctionComponent<Props> = ({setRomLoaded}) => {
-    const {
-        play: playGba,
-    } = useContext(GbaContext);
+const FetchROM: FunctionComponent<Props> = ({ setRomLoaded }) => {
+  const {
+    play: playGba,
+  } = useContext(GbaContext);
 
-    const getGameRom = async (url: string): Promise<Uint8Array> => {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Failed fetching ROM')
-        }
-        const buffer = await response.arrayBuffer();
-        return new Uint8Array(buffer)
+  const getGameRom = async (url: string): Promise<Uint8Array> => {
+    const response = await fetch(url);
+    const buffer = await response.arrayBuffer();
+    if (!response.ok) {
+      throw new Error('Failed fetching ROM')
+    }
+    return new Uint8Array(buffer)
+  }
+
+  const load = async () => {
+    const romurl = await axios.post("/api/load", {
+      contract_address: "0x1234"
+    });
+    if (romurl.data.error) {
+      return console.log(romurl.data.error);
     }
 
-    const load = async () => {
-        const grom = await getGameRom("http://localhost:3000/mario.gba");
-        if (!grom) {
-            throw new Error("fetched ROM is NULL or undefined")
-        } else {
-            setRomLoaded(true);
-            playGba({ newRomBuffer: grom });
-        }
+    console.log(romurl.data);
+
+    const grom = await getGameRom(romurl.data);
+
+    if (!grom) {
+      throw new Error("fetched ROM is NULL or undefined")
+    } else {
+      setRomLoaded(true);
+      playGba({ newRomBuffer: grom });
     }
+  }
 
-    useMemo(() => {
-        load();
-    }, []);
+  useMemo(() => {
+    load();
+  }, []);
 
-    return (
-        <>
-            {/* {!loaded && <p>Loading ...</p>} */}
-        </>
-    );
+  return (
+    <>
+      {/* {!loaded && <p>Loading ...</p>} */}
+    </>
+  );
 }
 
 export default FetchROM;
