29bb31bef95349106ac9e161981aa67831daa4e6
sandyagu <santiago.appu@gmail.com>
2024-06-22 02:02:22 +0530

    active listing done

--- packages/nextjs/app/api/create/erc20/route.tsx
+++ packages/nextjs/app/api/create/erc20/route.tsx
@@ -1,28 +1,28 @@
 import { RpcProvider, Account } from "starknet";
-
 const provider = new RpcProvider({
-  nodeUrl:
-    "https://rpc.nethermind.io/sepolia-juno?apikey=0Yp6vozXA3H5Codc8VkS22XB1bkBzgV1G4Isr9yJi19ngmpJ",
-});
-const privateKey0 =
-  "0x022c19376574847739db37f79db335f807d079368974acc1010698ab70422027";
-const account0Address: string =
-  "0x00a3eF13C5bEC03E5996fe3ce60D46905B63afadb2686759Cf325B1D01Cc6C62";
-const account0 = new Account(provider, account0Address, privateKey0);
-const erc20TokenAddress =
-  "0x01a6b7a2692f24ab772f330315bb56a485daba8a72c27fe996d275178f19c1e8";
-
+    nodeUrl:
+      "https://rpc.nethermind.io/sepolia-juno?apikey=0Yp6vozXA3H5Codc8VkS22XB1bkBzgV1G4Isr9yJi19ngmpJ",
+  });
+  const privateKey0 =
+    "0x022c19376574847739db37f79db335f807d079368974acc1010698ab70422027";
+  const account0Address: string =
+    "0x00a3eF13C5bEC03E5996fe3ce60D46905B63afadb2686759Cf325B1D01Cc6C62";
+  const account0 = new Account(provider, account0Address, privateKey0);
+  const erc20TokenAddress =
+    "0x01a6b7a2692f24ab772f330315bb56a485daba8a72c27fe996d275178f19c1e8";
+    
 function toHex(input: number | string): string {
   if (typeof input === "number") {
-    return input.toString(16);
+    return "0x" + input.toString(16);
   } else if (typeof input === "string") {
-    return Array.from(input)
+    return "0x" + Array.from(input)
       .map((char) => char.charCodeAt(0).toString(16))
-      .join(" ");
+      .join('');
   } else {
     throw new TypeError("Input must be a number or a string");
   }
 }
+
 
 const launchToken = async (
   token_supply: number,
@@ -30,16 +30,21 @@ const launchToken = async (
   symbol: string,
   addr: string
 ) => {
+    console.log(toHex(token_supply))
+    console.log(toHex(token_name))
+    console.log(toHex(symbol))
+  console.log(provider.getChainId());
   const reciept = await account0.deployContract({
     classHash: erc20TokenAddress,
     constructorCalldata: [
-      0x0186a0,
+      "0x0186a0",
       toHex(token_supply),
-      toHex(addr),
+      addr,
       toHex(token_name),
       toHex(symbol),
     ],
   });
+  console.log(reciept.transaction_hash);
   provider.waitForTransaction(reciept.transaction_hash);
 
   return reciept.contract_address;
--- packages/nextjs/app/api/create/route.tsx
+++ packages/nextjs/app/api/create/route.tsx
@@ -1,29 +1,79 @@
 import axios from "axios";
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
+import { error } from "console";
 
-const supabaseUrl = "https://mmrrmleeydqgkmkncqan.supabase.co"
-const supabasekey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tcnJtbGVleWRxZ2tta25jcWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5ODMxODEsImV4cCI6MjAzNDU1OTE4MX0.4BokD3hAhtepArvEK6Wzosr0LSVsIrkX25uj4Y_R8x4'
-const supabase = createClient(supabaseUrl,supabasekey)
-
-const launchToken = async (token_supply:Number,
-    token_name:String,
-    symbol:String,
-    addr:String) => {
+const supabaseUrl = "https://mmrrmleeydqgkmkncqan.supabase.co";
+const supabasekey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tcnJtbGVleWRxZ2tta25jcWFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5ODMxODEsImV4cCI6MjAzNDU1OTE4MX0.4BokD3hAhtepArvEK6Wzosr0LSVsIrkX25uj4Y_R8x4";
+const supabase = createClient(supabaseUrl, supabasekey);
 
 
-  };
+const get_newData=(current_data:number[],token_id:number,method:string)=>{
+    let new_data=current_data
+    if(method==="add"){
+        new_data.push(token_id)
+    }
+    else if(method==="remove"){
+        new_data=new_data.filter(item=>item!==token_id)
+    }
+    return new_data
+
+}
+
+
+const updateSupabase = async (token_id: number,method:string) => {
+  let { data: Starhack, error } = await supabase
+    .from("Starhack")
+    .select("active_listings")
+    .eq("id", 1);
+    if (error) {
+      console.log(error);
+      return { error };
+    }
+    else if (Starhack){
+        const current_data= (Starhack[0]?.active_listings)
+        const new_data=get_newData(current_data,token_id,method)
+        console.log(typeof(new_data))
+        const {data,error}=await supabase.from("Starhack").update([{active_listings:new_data}]).eq("id",1)
+        if (error) {
+          console.log(error);
+          return { error };
+        }
+    }
+    else return null
+    
+};
+
+const get_activeListing = async()=>{
+
+  let { data: Starhack, error } = await supabase
+    .from("Starhack")
+    .select("active_listings")
+    .eq("id", 1);
+    if (error) {
+      console.log(error);
+      return { error };
+    }
+    if (Starhack){
+        return Starhack[0]?.active_listings
+    }
+    else {
+        return null
+    }
+}
 
 export async function POST(req: Request) {
-    const formdata = await req.formData()
-    const token_supply = Number(formdata.get("supply"))
-    const token_name = String(formdata.get("name"))
-    const symbol = String(formdata.get("symbol"))
-    const addr =String( formdata.get("addr"))
-
-    const res = await launchToken(token_supply,
-        token_name,
-        symbol,
-        addr)
-    return Response.json(res)
+  const formdata = await req.formData();
+  const token_id = Number(formdata.get("token_id"));
+  const method = String(formdata.get("method"))
+  if (!method || !token_id){
+    return error
+  }
+    if (method=="get"){
+        return Response.json(await get_activeListing())
+    }
+    else
+ { const res = await updateSupabase(token_id,method);
+  return Response.json(res);}
 }
