5c84e6c8e871e801e01e9b24976746911161c3a0
abdxzi <azeezonline2013@gmail.com>
2024-06-22 10:47:08 +0530

    Add token in Create Form

--- packages/nextjs/components/CreateGameForm.tsx
+++ packages/nextjs/components/CreateGameForm.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { useState } from "react";
+
 export default function CreateGameForm() {
+
+    const [isChecked, setIsChecked] = useState(false);
+
     return (
         <div className="createGame flex bg-yellow justify-center flex-col md:flex-row mx-[30px] my-[50px] md:my-[100px]">
             {/* UPLOAD FILE */}
@@ -33,9 +38,21 @@ export default function CreateGameForm() {
                     <label htmlFor="price" className="mb-[0.5rem]">Item Price</label>
                     <input id="price" type="text" placeholder="Digital Awesome game" />
                 </div>
-                <div className="input-box">
-                    <label htmlFor="size" className="mb-[0.5rem]">Size</label>
-                    <input id="size" type="text" placeholder="Digital Awesome game" />
+                <div className="input-box flex items-center">
+                    <input id="token" type="checkbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
+                    <label htmlFor="token" className="ml-[10px]">Do you want to deploy token ?</label>
+                </div>
+                <div className={`input-box ${isChecked ? 'flex flex-col' : 'hidden'}`}>
+                    <label htmlFor="token-name" className="mb-[0.5rem]">Token Name</label>
+                    <input id="token-name" type="text" placeholder="Tether" />
+                </div>
+                <div className={`input-box ${isChecked ? 'flex flex-col' : 'hidden'}`}>
+                    <label htmlFor="token-symbol" className="mb-[0.5rem]">Token Symbol</label>
+                    <input id="token-symbol" type="text" placeholder="USDT" />
+                </div>
+                <div className={`input-box ${isChecked ? 'flex flex-col' : 'hidden'}`}>
+                    <label htmlFor="total-supply" className="mb-[0.5rem]">Token Supply</label>
+                    <input id="total-supply" type="text" placeholder="1000000" />
                 </div>
 
                 <button className="submit-button">Submit</button>
