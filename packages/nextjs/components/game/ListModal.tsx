7eea84dfe70fe063983080a640f1a6bb663577a9
sandyagu <santiago.appu@gmail.com>
2024-06-23 17:53:43 +0530

    final push

--- packages/nextjs/components/game/ListModal.tsx
+++ packages/nextjs/components/game/ListModal.tsx
@@ -21,12 +21,16 @@ const  ListNowModal:FunctionComponent<Props> = ({ game })=> {
         ],
       });
     const formData=new FormData()
+    
     const list_nft = async () => {
+        console.log({price})
         if (game.id && price && game.connectedAddress) {
-          list();
+          await list();
         }
         formData.append("token_id",String(game.id))
         formData.append("price",String(price))
+        formData.append("seller_address",String(game.connectedAddress))
+        formData.append("method","add")
         await axios.post("/api/create",formData)
       };
     
@@ -37,9 +41,9 @@ const  ListNowModal:FunctionComponent<Props> = ({ game })=> {
                 <span className="flex text-sm text-[#ffffff94]">List your games in Marketplace</span>
                 <div className="flex flex-col w-full max-w-[80%] mt-[30px]">
                     <label className="mb-[8px] text-[#ffffff]" htmlFor="list-price">Listing price</label>
-                    <input className="p-[10px] rounded-[5px] bg-[#ffffffed] text-[#000]" id="list-price" type="text" placeholder="0.002 ETH" onClick={(e) => { const target = e.target as HTMLInputElement; setPrice(Number(target.value)); }} />
+                    <input className="p-[10px] rounded-[5px] bg-[#ffffffed] text-[#000]" id="list-price" type="text" placeholder="0.002 ETH" onChange={(e)=>{setPrice(Number(e.target.value))}}  />
                 </div>
-                <div className="flex justify-center w-full py-[10px] bg-[#0058d2] hover:bg-[#1966d1] rounded-[10px] font-extrabold text-[1em] cursor-pointer max-w-[80%] mt-[25px]" onClick={()=>{list_nft()}}>
+                <div className="flex justify-center w-full py-[10px] bg-[#0058d2] hover:bg-[#1966d1] rounded-[10px] font-extrabold text-[1em] cursor-pointer max-w-[80%] mt-[25px]" onClick={list_nft}>
                     List
                 </div>
             </div>
