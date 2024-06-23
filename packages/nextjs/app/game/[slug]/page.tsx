83d2ad896c40a424bd6bd5197be68147255d6f79
abdxzi <azeezonline2013@gmail.com>
2024-06-23 12:14:50 +0530

    Game page added

--- packages/nextjs/app/game/[slug]/page.tsx
+++ packages/nextjs/app/game/[slug]/page.tsx
@@ -1,15 +1,63 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 import Game from "~~/components/game/Game";
 
 interface Params {
-    slug: string;
+    slug: number;
 }
 
-const Main = ({ params: { slug } }: { params: Params })=> {
-    // slug == 'mario'
-    return(
+type GameType = {
+    id: number,
+    name: string,
+    description: string,
+    supply: number,
+    image: string
+}
+
+const Main = ({ params: { slug } }: { params: Params }) => {
+
+    const [gameMetadata, setGameMetadata] = useState<GameType | null>(null);
+
+    useEffect(() => {
+        fetchGameData(slug)
+    }, [slug])
+
+    const fetchGameData = async (game_id: number) => {
+        const data = {
+            id: game_id
+        }
+
+        try {
+            const res = await fetch('http://localhost:3000/api/findGameFromID', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            const _data = await res.json();
+            const game = _data.game[0];
+
+            const _game:GameType = {
+                id:game.id,
+                name:game.name,
+                description:game.metadata.description,
+                image: `https://ipfs.io/ipfs/${game.metadata.image.replace('ipfs://', '')}`,
+                supply:game.metadata.supply,
+            }
+
+            setGameMetadata(_game);
+
+        } catch (e) {
+            console.log("Error !");
+        }
+
+    }
+
+    return (
         <>
-            {slug == "mario" ? <Game /> : "Game Not Found"}
+            {gameMetadata && <Game game={gameMetadata} />}
         </>
     )
 }
